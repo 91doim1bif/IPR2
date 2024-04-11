645c2525c92fee64d2c03368a3a279efb0f034af
MIKAIL\mikai <meikel.61.55.99@gmail.com>
2024-04-11 21:33:28 +0200

    new login site

--- mikail/src/App.tsx
+++ mikail/src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container mt-3">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/user" element={<BoardUser />} />
+      <Route path="/mod" element={<BoardModerator />} />
+      <Route path="/admin" element={<BoardAdmin />} />
+    </Routes>
+  </div>
 }
 
 export default App;
